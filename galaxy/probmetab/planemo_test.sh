673e482c64d268057a624a0df8a27a877068f30a
lecorguille <lecorguille@users.noreply.github.com>
2016-05-26 15:47:15 +0200

    add zip in the inputs

--- galaxy/probmetab/planemo_test.sh
+++ galaxy/probmetab/planemo_test.sh
@@ -2,4 +2,4 @@
 # -- Use of conda dependencies
 planemo conda_init --conda_prefix /tmp/mc
 planemo conda_install --conda_prefix /tmp/mc .
-planemo test --install_galaxy --conda_prefix /tmp/mc --conda_dependency_resolution --update_test_data
+planemo test --install_galaxy --conda_prefix /tmp/mc --conda_dependency_resolution 
